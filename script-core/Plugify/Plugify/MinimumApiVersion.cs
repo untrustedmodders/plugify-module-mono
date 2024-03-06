a762701616594aabc8eddce55e96d118fa814da7
Nikita Ushakov <ushakovn@tcd.ie>
2024-03-06 02:25:22 +0000

    Add minver

---
+++ script-core/Plugify/Plugify/MinimumApiVersion.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Plugify
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class MinimumApiVersion : System.Attribute
+    {
+        public int Version { get; }
+
+        /// <summary>
+        /// API version that this plugin requires to work correctly.
+        /// </summary>
+        /// <param name="version"></param>
+        public MinimumApiVersion(int version)
+        {
+            Version = version;
+        }
+    }
+}
