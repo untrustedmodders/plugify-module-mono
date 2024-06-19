abf7b2d123c4d38151c92c23b222238ad1a49fe3
Nikita Ushakov <nikita.ushakov0000@outlook.com>
2024-06-19 20:50:15 +0100

    Spaces to tabs

--- managed/ExamplePlugin/ExamplePlugin.cs
+++ managed/ExamplePlugin/ExamplePlugin.cs
@@ -4,63 +4,63 @@ using Plugify;
 
 namespace example_plugin
 {
-    public static class example_plugin 
-    {
-        public delegate void MyFunc(int a, string c);
+	public static class example_plugin 
+	{
+		public delegate void MyFunc(int a, string c);
 		
-       // [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        ////internal static extern void MakePrint(int i, string c);
+	   // [MethodImplAttribute(MethodImplOptions.InternalCall)]
+		////internal static extern void MakePrint(int i, string c);
 		
-       // [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        //internal static extern void ReceiveFuncDelegate(MyFunc f);
-    }
+	   // [MethodImplAttribute(MethodImplOptions.InternalCall)]
+		//internal static extern void ReceiveFuncDelegate(MyFunc f);
+	}
 }
 
 namespace ExamplePlugin
 {
-    public class SamplePlugin : Plugin
-    {
-        /**
+	public class SamplePlugin : Plugin
+	{
+		/**
 		 * Called when the plugin is fully initialized and all known external dependencies.
 		 * This is only called once in the lifetime of the plugin, and is paired with OnEnd().
 		 */
-        void OnStart()
-        {
-            int i = 0;
-            int b = 2;
-            int c = i + b;
+		void OnStart()
+		{
+			int i = 0;
+			int b = 2;
+			int c = i + b;
 
-            string S = "name";
+			string S = "name";
 
-            S += c;
+			S += c;
 
-            string ddc = S;
+			string ddc = S;
 
-            Console.Write($"{Name}: OnStart\n");
-            Console.Write(S + "\n");
-            
-            
-            string[] lines = {"some text1", "some text2", "some text3"};
-            File.WriteAllLines(@"someText.txt", lines);
-            
-            //example_plugin.example_plugin.MakePrint(3);
-            //example_plugin.example_plugin.ReceiveFuncDelegate(MyExportFunction);
-            //example_plugin.example_plugin.ReceiveFuncDelegate(MakePrint);
-        }
+			Console.Write($"{Name}: OnStart\n");
+			Console.Write(S + "\n");
+			
+			
+			string[] lines = {"some text1", "some text2", "some text3"};
+			File.WriteAllLines(@"someText.txt", lines);
+			
+			//example_plugin.example_plugin.MakePrint(3);
+			//example_plugin.example_plugin.ReceiveFuncDelegate(MyExportFunction);
+			//example_plugin.example_plugin.ReceiveFuncDelegate(MakePrint);
+		}
 
-        /**
+		/**
 		 * Called when the plugin is about to be unloading and all known external dependencies are still exist.
 		 */
-        void OnEnd()
-        {
-            //Console.Write($"{Name}: OnEnd\n");
-        }
+		void OnEnd()
+		{
+			//Console.Write($"{Name}: OnEnd\n");
+		}
 
-        void MyExportFunction(int a, string c)
-        {
-            //Console.Write($"{Name}: OnEnd\n");
-            //Console.Write("I believe that what doesn't kill you makes you... stranger!!! \n");
-           // Console.Write($"{a}, {c}");
-        }
-    }
+		void MyExportFunction(int a, string c)
+		{
+			//Console.Write($"{Name}: OnEnd\n");
+			//Console.Write("I believe that what doesn't kill you makes you... stranger!!! \n");
+		   // Console.Write($"{a}, {c}");
+		}
+	}
 }
