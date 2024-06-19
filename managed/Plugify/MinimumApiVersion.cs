6078ab45f1070c71f7b3a5e0897e0ddb8aebfd33
Nikita Ushakov <nikita.ushakov0000@outlook.com>
2024-06-19 20:50:35 +0100

    Changes in rest of files

--- managed/Plugify/MinimumApiVersion.cs
+++ managed/Plugify/MinimumApiVersion.cs
@@ -2,18 +2,18 @@ using System;
 
 namespace Plugify
 {
-    [AttributeUsage(AttributeTargets.Class)]
-    public class MinimumApiVersion : System.Attribute
-    {
-        public int Version { get; }
+	[AttributeUsage(AttributeTargets.Class)]
+	public class MinimumApiVersion : System.Attribute
+	{
+		public int Version { get; }
 
-        /// <summary>
-        /// API version that this plugin requires to work correctly.
-        /// </summary>
-        /// <param name="version"></param>
-        public MinimumApiVersion(int version)
-        {
-            Version = version;
-        }
-    }
+		/// <summary>
+		/// API version that this plugin requires to work correctly.
+		/// </summary>
+		/// <param name="version"></param>
+		public MinimumApiVersion(int version)
+		{
+			Version = version;
+		}
+	}
 }
